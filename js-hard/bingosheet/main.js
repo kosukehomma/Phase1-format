740d0a8eae00a12ef0a7820846c29f22c8001df1
kosukehomma <kou4321ske@gmail.com>
2022-10-19 19:37:58 +0900

    JavaScript上級　JS修正

--- js-hard/bingosheet/main.js
+++ js-hard/bingosheet/main.js
@@ -48,115 +48,100 @@ const [
   bCol5, iCol5, nCol5, gCol5, oCol5,
 ] = colArea;
 
-const bColRandomNum = () => {
-  let arr = [];
-  numArr = [];
+const numArr = [];
+
+const colRandomNum = () => {
+  let arrB = [];
+  let arrI = [];
+  let arrN = [];
+  let arrG = [];
+  let arrO = [];
   for(let i = 0; i < 15; i++) {
-    arr[i] = i + 1;
+    arrB[i] = i + 1;
+    arrI[i] = (i + 15) + 1;
+    arrN[i] = (i + 30) + 1;
+    arrG[i] = (i + 45) + 1;
+    arrO[i] = (i + 60) + 1;
   }
-  for(let j = 0, len = arr.length; j < 5; j++, len--) {
-    let rndNum = Math.floor(Math.random() * len);
-    numArr.push(arr[rndNum]);
-    arr[rndNum] = arr[len -1];
+
+  for(let b = 0, lenB = arrB.length; b < 5; b++, lenB--) {
+    let rndNum = Math.floor(Math.random() * lenB);
+    numArr.push(arrB[rndNum]);
+    arrB[rndNum] = arrB[lenB -1];
   }
+  for(let i = 0, lenI = arrI.length; i < 5; i++, lenI--) {
+    let rndNum = Math.floor(Math.random() * lenI);
+    numArr.push(arrI[rndNum]);
+    arrI[rndNum] = arrI[lenI - 1];
+  }
+  for(let n = 0, lenN = arrN.length; n < 4; n++, lenN--) {
+    let rndNum = Math.floor(Math.random() * lenN);
+    numArr.push(arrN[rndNum]);
+    arrN[rndNum] = arrN[lenN - 1];
+  }
+  for(let g = 0, lenG = arrG.length; g < 5; g++, lenG--) {
+    let rndNum = Math.floor(Math.random() * lenG);
+    numArr.push(arrG[rndNum]);
+    arrG[rndNum] = arrG[lenG - 1];
+  }
+  for(let o = 0, lenO = arrO.length; o < 5; o++, lenO--) {
+    let rndNum = Math.floor(Math.random() * lenO);
+    numArr.push(arrO[rndNum]);
+    arrO[rndNum] = arrO[lenO - 1];
+  }
+
   bCol1.textContent = numArr[0];
   bCol2.textContent = numArr[1];
   bCol3.textContent = numArr[2];
   bCol4.textContent = numArr[3];
   bCol5.textContent = numArr[4];
-};
-
-const iColRandomNum = () => {
-  let arr = [];
-  numArr = [];
-  for(let i = 15; i < 30; i++) {
-    arr[i] = i + 1;
-  }
-  console.log(arr);
-  for(let j = 0, len = arr.length; j < 5; j++, len--) {
-    let rndNum = Math.floor(Math.random() * (30 - 15) + 15);
-    numArr.push(arr[rndNum]);
-    arr[rndNum] = arr[len - 1];
-  }
-  console.log(numArr);
-  iCol1.textContent = numArr[0];
-  iCol2.textContent = numArr[1];
-  iCol3.textContent = numArr[2];
-  iCol4.textContent = numArr[3];
-  iCol5.textContent = numArr[4];
-};
-
-const nColRandomNum = () => {
-  let arr = [];
-  numArr = [];
-  for(let i = 30; i < 45; i++) {
-    arr[i] = i + 1;
-  }
-  console.log(arr);
-  for(let j = 0, len = arr.length; j < 4; j++, len--) {
-    let rndNum = Math.floor(Math.random() * (45 - 30) + 30);
-    numArr.push(arr[rndNum]);
-    arr[rndNum] = arr[len - 1];
-  }
-  console.log(numArr);
-  nCol1.textContent = numArr[0];
-  nCol2.textContent = numArr[1];
+  
+  iCol1.textContent = numArr[5];
+  iCol2.textContent = numArr[6];
+  iCol3.textContent = numArr[7];
+  iCol4.textContent = numArr[8];
+  iCol5.textContent = numArr[9];
+  
+  nCol1.textContent = numArr[10];
+  nCol2.textContent = numArr[11];
   nCol3.textContent = 'FREE';
-  nCol4.textContent = numArr[2];
-  nCol5.textContent = numArr[3];
+  nCol3.classList.add('hitNum')
+  nCol4.textContent = numArr[12];
+  nCol5.textContent = numArr[13];
+
+  gCol1.textContent = numArr[14];
+  gCol2.textContent = numArr[15];
+  gCol3.textContent = numArr[16];
+  gCol4.textContent = numArr[17];
+  gCol5.textContent = numArr[18];
+  
+  oCol1.textContent = numArr[19];
+  oCol2.textContent = numArr[20];
+  oCol3.textContent = numArr[21];
+  oCol4.textContent = numArr[22];
+  oCol5.textContent = numArr[23];
 };
 
-const gColRandomNum = () => {
-  let arr = [];
-  numArr = [];
-  for(let i = 45; i < 60; i++) {
-    arr[i] = i + 1;
-  }
-  console.log(arr);
-  for(let j = 0, len = arr.length; j < 5; j++, len--) {
-    let rndNum = Math.floor(Math.random() * (60 - 45) + 45);
-    numArr.push(arr[rndNum]);
-    arr[rndNum] = arr[len - 1];
-  }
-  console.log(numArr);
-  gCol1.textContent = numArr[0];
-  gCol2.textContent = numArr[1];
-  gCol3.textContent = numArr[2];
-  gCol4.textContent = numArr[3];
-  gCol5.textContent = numArr[4];
-};
-
-const oColRandomNum = () => {
-  let arr = [];
-  numArr = [];
-  for(let i = 60; i < 75; i++) {
-    arr[i] = i + 1;
-  }
-  console.log(arr);
-  for(let j = 0, len = arr.length; j < 5; j++, len--) {
-    let rndNum = Math.floor(Math.random() * (75 - 60) + 60);
-    numArr.push(arr[rndNum]);
-    arr[rndNum] = arr[len - 1];
-  }
-  console.log(numArr);
-  oCol1.textContent = numArr[0];
-  oCol2.textContent = numArr[1];
-  oCol3.textContent = numArr[2];
-  oCol4.textContent = numArr[3];
-  oCol5.textContent = numArr[4];
-};
-
-bColRandomNum();
-iColRandomNum();
-nColRandomNum();
-gColRandomNum();
-oColRandomNum();
+colRandomNum();
+console.log(numArr);
 
 const setBtn = document.getElementById('hitNum');
 setBtn.addEventListener('click', () => {
-  bColRandomNum();
-  iColRandomNum();
-  nColRandomNum();
-  gColRandomNum();
-  oColRandomNum();
+  const callNum = Math.floor(Math.random() * 75 + 1);
+
+  const bingoNum = numArr.indexOf(callNum);
+  console.log('alertの番号があるか',bingoNum);
+  console.log('取り出した数値',numArr[bingoNum]);
+  
+  alert(`次の番号は${callNum}番です。`);
+
+  console.log(callNum === numArr[bingoNum]);
+  if(callNum === numArr[bingoNum]) {
+    colArea[bingoNum].classList.add('hitNum');
+    // numArr.splice(bingoNum, 1);
+    // delete numArr[bingoNum];
+    // console.log(numArr);
+  } else {
+    return;
+  }
 });
